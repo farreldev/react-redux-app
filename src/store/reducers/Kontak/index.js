872bbde4d3d68df3657f0d14b0a1cd5f4fceacf7
office PC <teddywijaya93@gmail.com>
2023-02-28 17:10:29 +0700

    save reducer modify

--- src/store/reducers/Kontak/index.js
+++ src/store/reducers/Kontak/index.js
@@ -8,6 +8,7 @@ const initialState = {
 const Kontak = (state = initialState, action) => {
 	switch (action.type) {
 		case GET_LIST_CONTACT:
+			console.log("4. Masuk Reducer", action);
 			return { ...state, getListContactResult: action.payload.data, getListContactLoading: action.payload.loading, getListContactError: action.payload.errorMessage };
 		default:
 			return state;
